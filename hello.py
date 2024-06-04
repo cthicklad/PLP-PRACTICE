de7ed3f08f592d545dcb2d1c1d3a1c8281c2b887
cthicklad <thicklad32288@gmail.com>
2024-06-04 13:56:43 +0100

    created a python file

---
+++ hello.py
@@ -0,0 +1,6 @@
+#Press letter i to insert
+#Create a code inside the file
+#Press Esc and :wq (esc:wq), w=write, q=quit/save
+#!/usr/bin/python3 (this means go to user, then bin and find python3)
+Print("Hello World Fruitful and Isaac")
+
